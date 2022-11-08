6fdba6e57669b7fe34060f0dd51560e0b8a93907
aEnchev669 <aenchev669@abv.bg>
2022-11-08 13:37:10 +0300

    Encapsulation

---
+++ Lab Encapsulation/1. Sort Persons by Name and Age/1. Sort Persons by Name and Age/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var person = new Person(input[0], input[1], int.Parse(input[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(n => n.FirstName)
+                .ThenBy(n => n.Age)
+                .ToList()
+                .ForEach(n => Console.WriteLine(n.ToString()));
+
+        }
+    }
+}
---
+++ Lab Encapsulation/4. First and Reserve Team/4. First and Reserve Team/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                try
+                {
+                    string[] input = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    var person = new Person(input[0], input[1], int.Parse(input[2]), decimal.Parse(input[3]));
+                    persons.Add(person);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+            
+
+           
+         
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}

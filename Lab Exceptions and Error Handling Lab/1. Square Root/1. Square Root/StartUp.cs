052e6c565939c69a7d1e6c6375e2b7022482a057
aEnchev669 <aenchev669@abv.bg>
2022-11-15 17:50:27 +0300

    Exceptions

---
+++ Lab Exceptions and Error Handling Lab/1. Square Root/1. Square Root/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._Square_Root
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if (number <= 0)
+                {
+                    throw new Exception("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Goodbye.");
+        }
+    }
+}

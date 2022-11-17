170f2aa924f3d7c93d25fbcd69d120cc497875c5
aEnchev669 <aenchev669@abv.bg>
2022-11-17 23:16:49 +0200

    Solid - Exercise

---
+++ SOLID - Exercises/Logger.ConsoleApp/Logger.Core/Layouts/Interfaces/ILayout.cs
@@ -0,0 +1,14 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Core.Layouts.Interfaces
+{
+    public interface ILayout
+    {
+        string Format { get; }
+        string FormatMessage();
+    }
+}
---
+++ SOLID - Exercises/Logger.ConsoleApp/Logger.Core/Models/Message.cs
@@ -0,0 +1,53 @@
+﻿using Logger.Core.Enums;
+using Logger.Core.Exceptions;
+using Logger.Core.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Core.Models
+{
+    public class Message : IMessage
+    {
+
+        private string messageText;
+        private string dateTime;
+
+        public Message(string messageText, string dataTime, ReportLevel reportLevel)
+        {
+            this.MessageText = messageText;
+
+        }
+        public string DateType
+        {
+            get
+            {
+                return this.dateTime;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public ReportLevel reportLevel { get; private set; }
+
+        public string MessageText
+        {
+            get
+            {
+                return this.DateType;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(messageText))
+                {
+                    throw new EmptyMessageTextException();
+                }
+                this.messageText = value;
+            }
+        }
+    }
+}
